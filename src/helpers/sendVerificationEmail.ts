9b62d0b73bbd9249d39704f791e0b0531ec1571f
itsshiva78 <2022002786.shiva@ug.sharda.ac.in>
2025-08-06 19:36:10 +0530

    changes in verification

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,9 +9,9 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: 'dev@hiteshchoudhary.com',
+      from: 'dev@shivasrivastava.com',
       to: email,
-      subject: 'Mystery Message Verification Code',
+      subject: 'Whispers Within Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
     });
     return { success: true, message: 'Verification email sent successfully.' };
